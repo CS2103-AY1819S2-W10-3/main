38ced77b5c6edd5d55e092a277b9566b7bf22c97
LiuXuanIan <liuxuan2472@gmail.com>
2019-03-19 22:04:01 +0800

    list-w command and create some test files, more test cases.

--- src/test/java/seedu/address/model/equipment/UniqueWorkListListTest.java
+++ src/test/java/seedu/address/model/equipment/UniqueWorkListListTest.java
@@ -10,7 +10,7 @@ import static seedu.address.testutil.TypicalEquipments.BOB;
 
 //import java.util.Arrays;
 //import java.util.Collections;
-//import java.util.List;
+import java.util.List;
 
 import java.util.LinkedList;
 
@@ -53,6 +53,12 @@ public class UniqueWorkListListTest {
     public void contains_workListInList_returnsTrue() {
         uniqueWorkListList.add(typicalWorkListA);
         assertTrue(uniqueWorkListList.contains(typicalWorkListA));
+    }
+
+    @Test
+    public void canDoSortById() {
+        uniqueWorkListList.sortById();
+        assertTrue(true);
     }
 
     @Test
